670b40d22425a2329445da7b57ed9c7193bdfb28
Vuthy <vuthy.v@ligercambodia.org>
2018-07-13 13:58:55 +0700

    Add Sweetalert successfully

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -2,3 +2,15 @@ import './home.html';
 
 import '../../components/hello/hello.js';
 import '../../components/info/info.js';
+
+
+Template.App_home.events({
+  'click .block2-btn-addcart'(e) {
+    var nameProduct = "Vuthy"; // TODO: Get name of product
+    swal(nameProduct, "is added to cart !", "success"); // Swal is sweet alert function
+  },
+  'click .block2-btn-addwishlist'(e) {
+    var nameProduct = "Vuthy"; // TODO: Get name of product
+    swal(nameProduct, "is added to wishlist !", "success");
+  }
+})
