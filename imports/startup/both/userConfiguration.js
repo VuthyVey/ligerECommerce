984c36b5298ad07f24db8fa60f628313636c81a6
Vuthy <vuthy.v@ligercambodia.org>
2018-07-15 14:11:45 +0700

    Move userConfiguration file to startup folder

---
+++ imports/startup/both/userConfiguration.js
@@ -0,0 +1,46 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+var myPostLogout = function() {
+  //example redirect after logout
+  FlowRouter.go('/');
+};
+
+var myPostSummit = function() {
+  //example redirect after logout
+  FlowRouter.go('/');
+};
+
+AccountsTemplates.configure({
+  onLogoutHook: myPostLogout,
+  onSubmitHook: myPostSummit,
+  //validation
+  continuousValidation: true,
+  // Redirects
+  homeRoutePath: '/',
+  redirectTimeout: 0,
+
+  texts: {
+    button: {
+      signUp: "Register",
+      signIn: "Login"
+    },
+    socialSignUp: "Register",
+    socialIcons: {
+      "meteor-developer": "fa fa-rocket"
+    },
+    title: {
+      forgotPwd: "Recover Your Password"
+    }
+  }
+});
+
+AccountsTemplates.addFields([{
+  _id: 'firstName',
+  type: 'text',
+  displayName: 'Username',
+  required: true,
+  minLength: 6,
+  placeholder: "Username",
+  re: /(?=.*[a-z])(?=.*[A-Z])/,
+  errStr: 'At least 1 lowercase and 1 uppercase letter required'
+}]);
