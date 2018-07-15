de39a41d35ad2adf47694d4d7da64cdf290c8ca1
Vuthy <vuthy.v@ligercambodia.org>
2018-07-15 14:02:05 +0700

    Organize layout pages

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,8 +1,10 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
-// Import needed templates
-import '../../ui/layouts/body/body.js';
+// Import needed layout template
+import '../../ui/layouts/shop/lShop.js';
+import '../../ui/layouts/admin/lAdmin.js';
+// Import needed pages template
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/admin/admin.js';
 import '../../ui/pages/admin/newProduct.js';
@@ -14,34 +16,34 @@ import '../../ui/pages/not-found/not-found.js';
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
+    BlazeLayout.render('lShop', { main: 'App_home' });
   },
 });
 
 FlowRouter.route('/login', {
   name: 'App.login',
   action() {
-    BlazeLayout.render('App_body', { main: 'App_login' });
+    BlazeLayout.render('lShop', { main: 'App_login' });
   },
 });
 
 FlowRouter.route('/admin', {
   name: 'App.admin',
   action() {
-    BlazeLayout.render('App_body_admin', { main: 'admin' });
+    BlazeLayout.render('lAdmin', { main: 'admin' });
   },
 });
 
 FlowRouter.route('/admin/new_product', {
   name: 'App.newProduct',
   action() {
-    BlazeLayout.render('App_body_admin', { main: 'App_newProduct' });
+    BlazeLayout.render('lAdmin', { main: 'App_newProduct' });
   },
 });
 
 
 FlowRouter.notFound = {
   action() {
-    BlazeLayout.render('App_body', { main: 'App_notFound' });
+    BlazeLayout.render('lShop', { main: 'App_notFound' });
   },
 };
