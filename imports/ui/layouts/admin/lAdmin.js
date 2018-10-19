d188db0381aca801aa7f726437f2ef00c6c20090
Vuthy <vuthy.v@ligercambodia.org>
2018-10-19 10:59:01 +0700

    Admin

--- imports/ui/layouts/admin/lAdmin.js
+++ imports/ui/layouts/admin/lAdmin.js
@@ -1,14 +1,5 @@
-// import template
-import './lAdmin.html';
-// import packages
-import { Meteor } from 'meteor/meteor';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-import { Roles } from 'meteor/alanning:roles';
-// import 'bootstrap';
+import './lAdmin.html'
 
-Template.lAdmin.onRendered(()=> {
-  $('head').append('<link rel="stylesheet" href="/assets/scss/style.css">');
-})
 Template.lAdmin.helpers({
   'subscriptionsReady' () {
     return FlowRouter.subsReady(); // return weather registered subscriptions done with its job
